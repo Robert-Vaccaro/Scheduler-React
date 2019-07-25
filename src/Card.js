5bee462249356e845fc80d0cfee8cf5813e07f01
Bobby <robertvaccaro@Bobbys-MacBook-Pro.local>
2019-07-25 10:27:46 -0700

    Initial Scheduler-React Commit

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+        onClick={() => props.onClickDelete(props.id)}
+      >
+        delete
+      </button>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  onClickDelete: () => {}
+};
